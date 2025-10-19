28692523427980f5d86900386e4c4e6a8a81b3ad
ayush-writes-code <jwalaladka@gmail.com>
2025-10-19 23:46:13 +0530

    Update arraylib.h

--- arraylib.h
+++ arraylib.h
@@ -4,10 +4,9 @@
 int findMaxIndex(int arr[], int size);
 int findMinIndex(int arr[], int size);
 float findAverage(int arr[], int size);
-void displayArray(int arr[], int size);
+void showArray(int arr[], int size);
 void reverseArray(int arr[], int size);
-void sortArray(int arr[], int size);
-int linearSearch(int arr[], int size, int value);
+void bubbleSort(int arr[], int size);
+int linearSearch(int arr[], int size, int val);
 
 #endif
-
