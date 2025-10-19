70819c6db1142bf3496720be51bb7db364175387
ayush-writes-code <jwalaladka@gmail.com>
2025-10-19 23:46:27 +0530

    Update arraylib.c

--- arraylib.c
+++ arraylib.c
@@ -2,57 +2,72 @@
 #include "arraylib.h"
 
 // #Cp
-void displayArray(int arr[], int size) {
-    printf("Array: ");
-    for (int i = 0; i < size; i++)
+// just display array
+void showArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
+    }
     printf("\n");
 }
 
+// find index of biggest value
 int findMaxIndex(int arr[], int size) {
-    int maxIdx = 0;
-    for (int i = 1; i < size; i++)
-        if (arr[i] > arr[maxIdx]) maxIdx = i;
-    return maxIdx;
+    int maxI = 0;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > arr[maxI])
+            maxI = i;
+    }
+    return maxI;
 }
 
+// find index of smallest value
 int findMinIndex(int arr[], int size) {
-    int minIdx = 0;
-    for (int i = 1; i < size; i++)
-        if (arr[i] < arr[minIdx]) minIdx = i;
-    return minIdx;
+    int minI = 0;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] < arr[minI])
+            minI = i;
+    }
+    return minI;
 }
 
+// find avg of all elements
 float findAverage(int arr[], int size) {
-    int sum = 0;
+    int total = 0;
     for (int i = 0; i < size; i++)
-        sum += arr[i];
-    return (float)sum / size;
+        total += arr[i];
+    return (float) total / size;
 }
 
+// reverse array elements
 void reverseArray(int arr[], int size) {
-    for (int i = 0, j = size - 1; i < j; i++, j--) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    int start = 0, end = size - 1;
+    while (start < end) {
+        int t = arr[start];
+        arr[start] = arr[end];
+        arr[end] = t;
+        start++;
+        end--;
     }
 }
 
-void sortArray(int arr[], int size) {
+// basic bubble sort
+void bubbleSort(int arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                int t = arr[j];
+                int temp = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j + 1] = t;
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-int linearSearch(int arr[], int size, int value) {
-    for (int i = 0; i < size; i++)
-        if (arr[i] == value)
+// linear search, returns index or -1
+int linearSearch(int arr[], int size, int val) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == val)
             return i;
+    }
     return -1;
 }
