39266c6e3be9eded9ba80cab0f041acd219e0059
ayush-writes-code <jwalaladka@gmail.com>
2025-10-21 16:36:56 +0530

    Update mylib.c

--- mylib.c
+++ mylib.c
@@ -2,57 +2,76 @@
 #include <math.h>
 #include "mylib.h"
 
-// #Cp
-// reverse number digits
-int reverseNum(int n) {
+
+
+int reverseNum(int n)
+{
     int rev = 0;
-    while (n > 0) {
-        rev = rev * 10 + n % 10;
-        n /= 10;
+    while(n > 0)
+    {
+        rev = rev * 10 + (n % 10);
+        n = n / 10;
     }
     return rev;
 }
 
-// check armstrong number
-int isArmstrong(int n) {
-    int temp = n, sum = 0, cntDigits = 0;
-    while (temp) {
-        cntDigits++;
+
+int isArmstrong(int n)
+{
+    int temp = n, sum = 0, count = 0;
+
+    while(temp > 0)
+    {
+        count++;
         temp /= 10;
     }
 
     temp = n;
-    while (temp) {
-        int rem = temp % 10;
-        sum += pow(rem, cntDigits);
+    while(temp > 0)
+    {
+        int digit = temp % 10;
+        sum += (int)round(pow(digit, count));
         temp /= 10;
     }
 
-    return (sum == n);
+    if(sum == n)
+        return 1;
+    else
+        return 0;
 }
 
-// check adams number
-int isAdams(int n) {
+
+int isAdams(int n)
+{
     int sq = n * n;
     int rev = reverseNum(n);
     int revSq = rev * rev;
-    // if reverse of square == square of reverse
-    return (reverseNum(sq) == revSq);
+
+    if(reverseNum(sq) == revSq)
+        return 1;
+    else
+        return 0;
 }
 
-// check prime number
-int isPrime(int n) {
-    if (n <= 1) return 0;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0)
+
+int isPrime(int n)
+{
+    if(n <= 1)
+        return 0;
+
+    for(int i = 2; i * i <= n; i++)
+    {
+        if(n % i == 0)
             return 0;
     }
+
     return 1;
 }
 
-// check number is both prime and palindrome
-int isPrimePalindrome(int n) {
-    if (isPrime(n) && n == reverseNum(n))
+
+int isPrimePalindrome(int n)
+{
+    if(isPrime(n) && n == reverseNum(n))
         return 1;
     else
         return 0;
